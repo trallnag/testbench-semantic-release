23fd75603436c641a6129ab175da4d5d72a5faf2
Tim Schwenke <tim.schwenke@trallnag.com>
2022-07-10 13:37:36 +0200

    ci(semantic-release): Adjust release-notes-generator type mapping

--- release.config.js
+++ release.config.js
@@ -48,17 +48,17 @@ module.exports = {
             // Visible.
             { type: 'feat', section: 'Features', hidden: false },
             { type: 'fix', section: 'Bug Fixes', hidden: false },
-            { type: 'perf', section: 'Performance', hidden: false },
             { type: 'revert', section: 'Reverts', hidden: false },
+            { type: 'perf', section: 'Performance', hidden: false },
             { type: 'refactor', section: 'Refactor', hidden: false },
             { type: 'test', section: 'Tests', hidden: false },
-            { type: 'docs', section: 'Docs', hidden: false },
+            { type: 'docs', section: 'Documentation', hidden: false },
+            { type: 'build', section: 'Build', hidden: false },
+            { type: 'ci', section: 'CI/CD', hidden: false },
+            { type: 'style', section: 'Styles', hidden: false },
 
             // Hidden.
-            { type: 'style', section: 'Styles', hidden: true },
             { type: 'chore', section: 'Chores', hidden: true },
-            { type: 'build', section: 'Build', hidden: true },
-            { type: 'ci', section: 'CI/CD', hidden: true },
           ],
         },
       },
